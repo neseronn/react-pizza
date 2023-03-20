bad802b041955ba0fa2cda490a5883edadebecf7
Авдеенко Елизавета ИВТ-301 <avdeenko.l26@mail.ru>
2023-03-20 19:19:07 +0300

    added async action, redux-thunk

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import axios from 'axios';
-import { setPizzas } from './redux/actions/pizzas';
+import { fetchPizzas } from './redux/actions/pizzas';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
@@ -11,10 +11,8 @@ function App() {
   const dispatch = useDispatch();
 
   React.useEffect(() => {
-    axios.get(process.env.REACT_APP_JSONSERVER_URL).then(({ data }) => {
-      dispatch(setPizzas(data));
-  });
-  });
+    dispatch(fetchPizzas());
+  }, []);
 
   return (
     <div className='wrapper'>
--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -1,3 +1,11 @@
+import axios from 'axios';
+
+export const fetchPizzas = () => (dispatch) => {
+  axios.get(process.env.REACT_APP_JSONSERVER_URL).then(({ data }) => {
+    dispatch(setPizzas(data));
+  });
+};
+
 export const setPizzas = (items) => ({
   type: 'SET_PIZZAS',
   payload: items,
